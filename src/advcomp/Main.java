3b074ad0f98e1e13d026882332fb827df76a8d53
charliecooke221 <charliecooke221@gmail.com>
2018-02-05 16:48:40 +0000

    Initial commit

---
+++ src/advcomp/Main.java
@@ -0,0 +1,11 @@
+package advcomp;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        ReaderCleaner reader = new ReaderCleaner();
+
+    }
+
+}
---
+++ src/advcomp/ReaderCleaner.java
@@ -0,0 +1,11 @@
+package advcomp;
+import java.io.BufferedInputStream;
+import java.io.File;
+
+
+public class ReaderCleaner {
+
+    String passengerDataLoc = "C:\\Users\\charlie\\IdeaProjects\\advanced_computing_map_reduce\\src\\advcomp\\AComp_Passenger_data.csv";
+    String airportsLatLongLoc = "C:\\Users\\charlie\\IdeaProjects\\advanced_computing_map_reduce\\src\\advcomp\\Top30_airports_LatLong.csv";
+
+}
